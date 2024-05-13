f9dcdbc7b401cb336f6fed4afb9f11105a7f1d1e
AvishkaGihan <avishkag18@gmail.com>
2024-05-13 19:33:59 +0530

    final commit

--- app/tickets/[id]/page.jsx
+++ app/tickets/[id]/page.jsx
@@ -1,3 +1,17 @@
+import { notFound } from "next/navigation";
+
+export const dynamicParams = true; // default val = true
+
+export async function generateStaticParams() {
+  const res = await fetch("http://localhost:4000/tickets");
+
+  const tickets = await res.json();
+
+  return tickets.map((ticket) => ({
+    id: ticket.id,
+  }));
+}
+
 async function getTicket(id) {
   const res = await fetch(`http://localhost:4000/tickets/${id}`, {
     next: {
@@ -5,10 +19,15 @@ async function getTicket(id) {
     },
   });
 
+  if (!res.ok) {
+    notFound();
+  }
+
   return res.json();
 }
 
 export default async function TicketDetails({ params }) {
+  // const id = params.id
   const ticket = await getTicket(params.id);
 
   return (
